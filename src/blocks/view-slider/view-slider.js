9ad854740f8471195a0358def92beb2b61a28e66
des-yogi <nuevo-design@mail.ru>
2023-04-20 21:19:23 +0300

    index in progress

--- src/blocks/view-slider/view-slider.js
+++ src/blocks/view-slider/view-slider.js
@@ -1,4 +1,12 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  const swiper = new Swiper('.view-slider__container', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+})();
